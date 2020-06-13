e23fc7628cf8d11a634115848d0e01de8351b6c0
kev <kev@localhost>
2020-06-14 03:17:46 +0800

    Auto commit: 2020-06-14_03:17:46

--- src/error/incorrect_answer.test.ts
+++ src/error/incorrect_answer.test.ts
@@ -3,7 +3,7 @@ import { Incorrect_answer } from './incorrect_answer';
 it('can throw', async () => {
   const correctness = 0;
   const answer = 'a';
-  const e = new Incorrect_answer({ correctness, answer });
+  const e = new Incorrect_answer({ correctness, answer, marker: 'test' });
   expect(e.message).toContain(answer);
   expect(e.result.correctness).toBe(correctness);
   expect(e.result.answer).toBe(answer);
